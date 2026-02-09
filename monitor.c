d297a1133c11574412a9c7ce47ebb0be1c0e83f1
DoubleZZeta <zhangao_tony@163.com>
2026-02-09 20:03:21 +0000

    update task2a

--- monitor.c
+++ monitor.c
@@ -8,11 +8,67 @@
 #include <pwd.h>        // getpwuid (convert UID to username)
 #include <ctype.h>      // isdigit
 
+unsigned long get_process_cpu_time(char* name)
+{
+    char path[512];
+    snprintf(path, sizeof(path), "/proc/%s/stat", name);
+    FILE *stat_file = fopen(path, "r");
+    if (stat_file == NULL)
+    {
+        perror("Failed to open status file");
+        return 0;
+    }
+
+    int pid;
+    char comm[256];
+    char state;
+    unsigned long utime, stime;
+    fscanf(stat_file, "%d %s %c", &pid, comm, &state);
+    for(int j = 0; j < 10; j++)
+    {
+        fscanf(stat_file, "%*s");
+    }
+    fscanf(stat_file, "%lu %lu", &utime, &stime);
+    fclose(stat_file);
+    
+    return utime + stime;
+}
+
+char *get_username_from_uid(char *name)
+{
+    char path[512];
+    int uid = -1;
+    
+    snprintf(path, sizeof(path), "/proc/%s/status", name);
+    FILE *status_file = fopen(path, "r");
+    if (status_file == NULL)
+    {
+        perror("Failed to open status file");
+        return 0;
+    }
+
+    char line[256];
+    while (fgets(line, sizeof(line), status_file))
+    {
+        if (strncmp(line, "Uid:", 4) == 0)
+        {
+            sscanf(line, "Uid:\t%d", &uid);
+            break;
+        }
+    }
+    fclose(status_file);
+
+    struct passwd *pw = getpwuid(uid);
+    char *username = pw ? pw->pw_name : "unknown"; // need to revise 
+    return username;
+}
+
 int main()
 {
     int num_users;
     char users[1024][256];
     unsigned long cpu_times[1024];
+    int user_count = 0;
     
     // Open the /proc directory 
     DIR *proc_dir = opendir("/proc");
@@ -43,27 +99,15 @@ int main()
 
             if(is_pid)
             {
-                char path[512];
-                snprintf(path, sizeof(path), "/proc/%s/stat", name);
-                FILE *status_file = fopen(path, "r");
-                if (status_file == NULL)
-                {
-                    perror("Failed to open status file");
-                    continue;
-                }
+                char *username = get_username_from_uid(name);
+                unsigned long cpu_time = get_process_cpu_time(name);
 
-                int pid;
-                char comm[256];
-                char state;
-                unsigned long utime, stime;
-                fscanf(status_file, "%d %s %c", &pid, comm, &state);
-                for(int j = 0; j < 10; j++)
-                {
-                    fscanf(status_file, "%*s");
-                }
-                fscanf(status_file, "%lu %lu", &utime, &stime);
-                printf("PID %d: utime=%lu stime=%lu total=%lu\n", pid, utime, stime, utime + stime);
-                fclose(status_file);
+                strncpy(users[user_count], username, sizeof(users[user_count]) - 1);
+                users[user_count][sizeof(users[user_count]) - 1] = '\0';
+                cpu_times[user_count] = cpu_time;
+                user_count++;
+                
+                printf("Process: %s, User: %s, CPU Time: %lu\n", name, username, cpu_time);
             }
         }
         else
